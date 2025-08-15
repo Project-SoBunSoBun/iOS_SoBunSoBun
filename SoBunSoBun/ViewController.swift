e47534155ae5578d39ad95fd3b55ae6361274d3f
heopill <heopill@naver.com>
2025-08-15 19:47:17 +0900

    Initial Commit

---
+++ SoBunSoBun/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SoBunSoBun
+//
+//  Created by 허성필 on 8/15/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
