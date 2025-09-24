fd50f91ec322c06ec8a0068fb8670a656acdf842
heopill <heopill@naver.com>
2025-09-24 22:20:07 +0900

    [Feat] #15 - AuthorizedAPI 추가

--- SoBunSoBun/NetworkManager/AuthorizedAPI.swift
+++ SoBunSoBun/NetworkManager/AuthorizedAPI.swift
@@ -5,4 +5,38 @@
 //  Created by 허성필 on 9/24/25.
 //
 
+
 import Foundation
+import Moya
+
+enum AuthorizedAPI {
+    
+}
+
+extension AuthorizedAPI: TargetType {
+    var baseURL: URL {
+        return URL(string: API_URL)!
+    }
+    
+    var path: String {
+        switch self {
+            
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+            
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+            
+        }
+    }
+    
+    var headers: [String : String]? {
+        return ["Content-Type": "application/json"]
+    }
+}
