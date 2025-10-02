686bcb7f1b867e13e96d74b47ed019a83fa865b0
heopill <heopill@naver.com>
2025-10-02 18:08:55 +0900

    [Feat] #17 - Font 구조 업데이트

--- SoBunSoBun/Fonts/Fonts.swift
+++ SoBunSoBun/Fonts/Fonts.swift
@@ -12,6 +12,8 @@ struct FontStyle {
     let fontSize: CGFloat
     let lineHeightMultiple: CGFloat // 행간 비율
     
+    var font: UIFont { return UIFont(name: fontName, size: fontSize)! } // .font을 위한 변수
+    
     var paragraphStyle: NSParagraphStyle {
         let style = NSMutableParagraphStyle()
         let lineHeight = fontSize * lineHeightMultiple
