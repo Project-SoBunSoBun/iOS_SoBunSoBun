8d16858715eefa422d237b95ab96949cf8825e5f
heopill <heopill@naver.com>
2025-10-16 22:37:58 +0900

    [Feat] #11 - 임시 토큰을 가져오는 메서드와 사용자 정보를 가져오는 메서드 추가

--- SoBunSoBun/NetworkManager/NetworkManager.swift
+++ SoBunSoBun/NetworkManager/NetworkManager.swift
@@ -21,10 +21,19 @@ final class NetworkManager {
     // Authorized API 전용
     private let authProvider = MoyaProvider<MultiTarget>(session: Session(interceptor: AuthInterceptor.shared))
     
-    // 서버에서 카카오 토큰을 통해 사용자 정보 가져오는 메서드
-    func fetchAuthLoginKakao(accessToken: String) -> Single<UserModel> {
+    // 서버에서 카카오 토큰을 통해 임시 토큰을 가져오는 메서드
+    func fetchAuthLoginKakao(accessToken: String) -> Single<KakaoAuthResponse> {
         return provider.rx.request(
             MultiTarget(PublicAPI.authLoginKakao(accessToken: accessToken))
+        )
+        .filterSuccessfulStatusCodes()
+        .map(KakaoAuthResponse.self)
+    }
+    
+    // 서버에서 임시 토큰을 통해 사용자 정보를 가져오는 메서드
+    func fetchAuthCompleteSignUp(loginToken: String, serviceTermsAgreed: Bool, privacyPolicyAgreed: Bool, marketingOptionalAgreed: Bool) -> Single<UserModel> {
+        return provider.rx.request(
+            MultiTarget(PublicAPI.authCompleteSignUp(loginToken: loginToken, serviceTermsAgreed: serviceTermsAgreed, privacyPolicyAgreed: privacyPolicyAgreed, marketingOptionalAgreed: marketingOptionalAgreed))
         )
         .filterSuccessfulStatusCodes()
         .map(UserModel.self)
