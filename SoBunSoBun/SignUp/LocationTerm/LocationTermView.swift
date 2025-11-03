296f50be5c0655d526afa2f6453b8db822b1fee6
heopill <heopill@naver.com>
2025-11-03 20:05:16 +0900

    [Feat] #11 - 임시 이용약관 디테일 화면 구현

---
+++ SoBunSoBun/SignUp/LocationTerm/LocationTermView.swift
@@ -0,0 +1,88 @@
+//
+//  LocationTermView.swift
+//  SoBunSoBun
+//
+//  Created by 허성필 on 10/23/25.
+//
+
+import UIKit
+import SnapKit
+import ReactorKit
+import RxSwift
+import RxCocoa
+
+class LocationTermView: CustomViewController {
+    typealias Reactor = LocationTermReactor
+    private let reactor = LocationTermReactor()
+    
+    private let disposeBag = DisposeBag()
+    
+    // 뒤로 가기 버튼
+    private let backButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "BlackLeft"), for: .normal)
+        
+        return button
+    }()
+
+    // 임시 타이틀
+    private let titleLabel: UILabel = {
+        let title = UILabel()
+        title.text = "Location 약관 화면"
+        title.textColor = .appleBlack
+        title.textAlignment = .center
+        
+        return title
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureUI()
+        bind(reactor: reactor)
+    }
+    
+    private func configureUI() {
+        view.backgroundColor = .appleWhite
+        
+        [backButton, titleLabel].forEach {
+            view.addSubview($0)
+        }
+        
+        backButton.snp.makeConstraints { make in
+            make.size.equalTo(48)
+            make.leading.equalToSuperview().offset(4)
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+    }
+}
+
+extension LocationTermView {
+    // reactor와 view 연결
+    func bind(reactor: LocationTermReactor) {
+        bindAction(reactor: reactor)
+        bindState(reactor: reactor)
+    }
+    
+    func bindAction(reactor: LocationTermReactor) {
+        // Back 버튼 탭
+        backButton.rx.tap
+            .map { Reactor.Action.backButtonTapped }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+    }
+    
+    func bindState(reactor: LocationTermReactor) {
+        // 뒤로 가기 버튼
+        reactor.pulse(\.$shouldPopViewController)
+            .compactMap { $0 }
+            .subscribe(onNext: { [weak self] _ in
+                self?.navigationController?.popViewController(animated: true)
+            })
+            .disposed(by: disposeBag)
+    }
+}
