17ba858a1539c7b151195a0dffe9326514b4f788
hugesilver <xor3160@naver.com>
2025-11-03 23:01:29 +0900

    [Design] #22 - InformationCard 추가

---
+++ SoBunSoBun/Components/InformationCard.swift
@@ -0,0 +1,159 @@
+//
+//  InformationCard.swift
+//  SoBunSoBun
+//
+//  Created by 김태은 on 11/3/25.
+//
+
+import UIKit
+import SnapKit
+
+class InformationCard: UIView {
+    init(frame: CGRect = .zero,
+         minCount: Int,
+         maxCount: Int,
+         location: String,
+         date: String) {
+        super.init(frame: frame)
+        
+        configure(minCount: minCount,
+                  maxCount: maxCount,
+                  location: location,
+                  date: date)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - 디자인 요소
+    // 행 stackview 컴포넌트
+    private func horizontalStackView() -> UIStackView {
+        let sv = UIStackView()
+        sv.axis = .horizontal
+        sv.spacing = 8
+        sv.distribution = .fill
+        
+        return sv
+    }
+    
+    // 제목 label 컴포넌트
+    private func titleLabel() -> UILabel {
+        let lb = UILabel()
+        lb.font = title16.font
+        lb.textColor = .neutral900
+        lb.numberOfLines = 1
+        lb.textAlignment = .left
+        
+        return lb
+    }
+    
+    // 설명 label 컴포넌트
+    private func descLabel() -> UILabel {
+        let lb = UILabel()
+        lb.font = body16.font
+        lb.textColor = .neutral700
+        lb.numberOfLines = 1
+        lb.textAlignment = .right
+        
+        return lb
+    }
+    
+    // 모집 인원
+    private lazy var participantsStackView: UIStackView = horizontalStackView()
+    private lazy var participantsTitleLabel: UILabel = {
+        let lb = titleLabel()
+        lb.text = String(localized: "NumberOfParticipants")
+        
+        return lb
+    }()
+    private lazy var participantsDescLabel: UILabel = descLabel()
+    
+    // 지점 위치
+    private lazy var marketLocationStackView: UIStackView = horizontalStackView()
+    private lazy var marketLocationTitleLabel: UILabel = {
+        let lb = titleLabel()
+        lb.text = String(localized: "MarketLocation")
+        
+        return lb
+    }()
+    private lazy var marketLocationDescLabel: UILabel = descLabel()
+    
+    // 날짜 및 시간
+    private lazy var dateTimeStackView: UIStackView = horizontalStackView()
+    private lazy var dateTimeTitleLabel: UILabel = {
+        let lb = titleLabel()
+        lb.text = String(localized: "DateTime")
+        
+        return lb
+    }()
+    private lazy var dateTimeDescLabel: UILabel = descLabel()
+    
+    // 마감일
+    private lazy var deadlineStackView: UIStackView = horizontalStackView()
+    private lazy var deadlineTitleLabel: UILabel = {
+        let lb = titleLabel()
+        lb.text = String(localized: "Deadline")
+        
+        return lb
+    }()
+    private lazy var deadlineDescLabel: UILabel = descLabel()
+    
+    // MARK: - 레이아웃 설정
+    private func configure(
+        minCount: Int,
+        maxCount: Int,
+        location: String,
+        date: String) {
+        self.backgroundColor = .primary50
+        self.layer.cornerRadius = 16
+        self.clipsToBounds = true
+        
+        // 모집 인원
+        participantsStackView.addArrangedSubview(participantsTitleLabel)
+        participantsStackView.addArrangedSubview(participantsDescLabel)
+        participantsDescLabel.text = String(format: NSLocalizedString("ParticipantCount", comment: "participants minimum and maximum counts"), minCount, maxCount)
+        
+        addSubview(participantsStackView)
+        
+        participantsStackView.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalToSuperview().inset(16)
+        }
+        
+        // 지점 위치
+        marketLocationStackView.addArrangedSubview(marketLocationTitleLabel)
+        marketLocationStackView.addArrangedSubview(marketLocationDescLabel)
+        marketLocationDescLabel.text = location
+        
+        addSubview(marketLocationStackView)
+        
+        marketLocationStackView.snp.makeConstraints { make in
+            make.top.equalTo(participantsStackView.snp.bottom).offset(8)
+            make.horizontalEdges.equalToSuperview().inset(16)
+        }
+        
+        // 날짜 및 시간
+        dateTimeStackView.addArrangedSubview(dateTimeTitleLabel)
+        dateTimeStackView.addArrangedSubview(dateTimeDescLabel)
+        dateTimeDescLabel.text = ISO8601ToLocalizedDateTimeString(date)
+            
+        addSubview(dateTimeStackView)
+            
+        dateTimeStackView.snp.makeConstraints { make in
+            make.top.equalTo(marketLocationStackView.snp.bottom).offset(8)
+            make.horizontalEdges.equalToSuperview().inset(16)
+        }
+        
+        // 마감일
+        deadlineStackView.addArrangedSubview(deadlineTitleLabel)
+        deadlineStackView.addArrangedSubview(deadlineDescLabel)
+        deadlineDescLabel.text = ISO8601ToDDay(date)
+            
+        addSubview(deadlineStackView)
+            
+        deadlineStackView.snp.makeConstraints { make in
+            make.top.equalTo(dateTimeStackView.snp.bottom).offset(8)
+            make.horizontalEdges.bottom.equalToSuperview().inset(16)
+        }
+    }
+}
--- SoBunSoBun/Utils.swift
+++ SoBunSoBun/Utils.swift
@@ -14,26 +14,67 @@ let API_URL = Bundle.main.object(forInfoDictionaryKey: "API_URL") as! String
 // 재발급 중
 var isRefreshing: Bool = false
 
-// 문자열에서 날짜 계산
+// 문자열에서 날짜 변환(토큰 만료 시간 계산에만 사용)
 func stringToDate(string: String, format: String) -> Date {
     let dateFormatter = DateFormatter()
     dateFormatter.dateFormat = format
     dateFormatter.locale = Locale(identifier: "ko_KR")
-    dateFormatter.amSymbol = "오전"
-    dateFormatter.pmSymbol = "오후"
     
     return dateFormatter.date(from: string)!
 }
 
-// 날짜에서 문자열 계산
+// 날짜에서 문자열 변환(토큰 만료 시간 계산에만 사용)
 func dateToString(date: Date, format: String) -> String {
     let dateFormatter = DateFormatter()
     dateFormatter.dateFormat = format
     dateFormatter.locale = Locale(identifier: "ko_KR")
-    dateFormatter.amSymbol = "오전"
-    dateFormatter.pmSymbol = "오후"
     
     return dateFormatter.string(from: date)
+}
+
+// ISO8601 Datetime에서 현지화 Datetime 문자열 변환
+func ISO8601ToLocalizedDateTimeString(_ isoDatetimeString: String) -> String {
+    let isoFormatter = ISO8601DateFormatter()
+    isoFormatter.formatOptions = [.withFullDate, .withFullTime]
+    
+    if let date = isoFormatter.date(from: isoDatetimeString) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.setLocalizedDateFormatFromTemplate("MMMd (E) a hh:mm")
+        
+        return dateFormatter.string(from: date)
+    } else {
+        print("isoFormatter.date 생성 중 오류 발생")
+        return "Error!"
+    }
+}
+
+// ISO8601 Datetime에서 D-Day 계산
+func ISO8601ToDDay(_ isoDatetimeString: String) -> String {
+    let isoFormatter = ISO8601DateFormatter()
+    isoFormatter.formatOptions = [.withFullDate, .withFullTime]
+    
+    if let date = isoFormatter.date(from: isoDatetimeString) {
+        let calendar = Calendar.current
+        let now = calendar.startOfDay(for: Date())
+        let targetDay = calendar.startOfDay(for: date)
+        
+        let components = calendar.dateComponents([.day], from: now, to: targetDay)
+        guard let day = components.day else {
+            print("components.day 생성 중 오류 발생")
+            return "Error!"
+        }
+        
+        if day > 0 {
+            return "D-\(day)"
+        } else if day == 0 {
+            return "D-Day"
+        } else {
+            return "D+\(-day)"
+        }
+    } else {
+        print("isoFormatter.date 생성 중 오류 발생")
+        return "Error!"
+    }
 }
 
 // 미리보기
