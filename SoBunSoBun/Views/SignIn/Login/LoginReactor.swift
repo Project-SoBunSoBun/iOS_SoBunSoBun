6d9f01545ef72bc1612d7e98aac93187aadc3522
heopill <heopill@naver.com>
2025-11-24 18:21:07 +0900

    [Fix] #35 - LoginReactor에 액세스, 리프레시 토큰을 저장하는 로직 추가

--- SoBunSoBun/Views/SignIn/Login/LoginReactor.swift
+++ SoBunSoBun/Views/SignIn/Login/LoginReactor.swift
@@ -20,16 +20,21 @@ class LoginReactor: Reactor {
     enum Action {
         case appleButtonTapped // 애플 로그인 버튼을 클릭했을 때
         case kakaoButtonTapped // 카카오 로그인 버튼을 클릭했을 때
+        case saveToken // 카카오 로그인 성공 시 토큰 저장하기
     }
     
     enum Mutation {
         case loginSuccess(isNewUser: Bool) // 로그인 성공했을 때
         case loginFailed(String) // 로그인에 실패했을 때
+        case saveSuccess(isSaved: Bool) // 키체인에 저장 성공했을 때
+        case saveFailed(String)
     }
     
     struct State {
         @Pulse var loginCompleted: Bool?
         @Pulse var loginErrorMessage: String?
+        @Pulse var saveSuccess: Bool?
+        @Pulse var saveFailedMessage: String?
     }
     
     func mutate(action: Action) -> Observable<Mutation> {
@@ -55,6 +60,8 @@ class LoginReactor: Reactor {
                 }
         case .appleButtonTapped:
             return Observable.empty()
+        case .saveToken:
+            return saveToken()
         }
     }
     
@@ -66,6 +73,10 @@ class LoginReactor: Reactor {
             newState.loginCompleted = isNewUser
         case .loginFailed(let message):
             newState.loginErrorMessage = message
+        case .saveSuccess(let isSaved):
+            newState.saveSuccess = isSaved
+        case .saveFailed(let message):
+            newState.saveFailedMessage = message
         }
         
         return newState
@@ -73,7 +84,7 @@ class LoginReactor: Reactor {
 }
 
 extension LoginReactor {
-    func kakaoLoginAction() -> Observable<OAuthToken> {
+    private func kakaoLoginAction() -> Observable<OAuthToken> {
         let loginObservable: Observable<OAuthToken>
         
         // KakaoTalk 앱을 통한 로그인
@@ -84,5 +95,29 @@ extension LoginReactor {
         }
         
         return loginObservable
+    }
+    
+    private func saveToken() -> Observable<Mutation> {
+        guard let loginToken = KeyChain.shared.get(key: "LOGIN_TOKEN") else {
+            return Observable.just(.saveFailed("로그인 토큰이 없습니다"))
+        }
+        
+        return NetworkManager.shared.fetchAuthCompleteSignUp(
+            loginToken: loginToken,
+            serviceTermsAgreed: true,
+            privacyPolicyAgreed: true,
+            marketingOptionalAgreed: false
+        )
+        .asObservable()
+        .map { userModel in
+            KeyChain.shared.set(key: "ACCESS_TOKEN", value: userModel.accessToken)
+            KeyChain.shared.set(key: "REFRESH_TOKEN", value: userModel.refreshToken)
+            KeyChain.shared.set(key: "ACCESS_TOKEN_EXPIRE_AT_KST", value: String(userModel.accessTokenExpiresAtKst))
+            KeyChain.shared.set(key: "REFRESH_TOKEN_EXPIRE_AT_KST", value: String(userModel.refreshTokenExpiresAtKst))
+            return Mutation.saveSuccess(isSaved: true)
+        }
+        .catch { error in
+            return Observable.just(.saveFailed("토큰 저장 실패"))
+        }
     }
 }
