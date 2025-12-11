e4ca4a61f6d4c84c1f4312a9418c1f952b46fe16
heopill <heopill@naver.com>
2025-12-11 23:54:58 +0900

    [Chore] #50 - PR 피드백에 따른 수정

--- SoBunSoBun/Views/SettleUp/SettleUp1stStep/SettleUp1stStepView.swift
+++ SoBunSoBun/Views/SettleUp/SettleUp1stStep/SettleUp1stStepView.swift
@@ -44,10 +44,10 @@ class SettleUp1stStepView: UIViewController {
             leading: 0,
             bottom: 0,
             trailing: 0)
+        config.image = .greyClose
         config.preferredSymbolConfigurationForImage = .init(pointSize: 24)
         
         button.configuration = config
-        button.setImage(UIImage(named: "BlackLeft"), for: .normal)
         
         return button
     }()
