9f2763c61176e272157d0a5d71ead1f561f7b0d1
heopill <heopill@naver.com>
2025-12-31 17:30:35 +0900

    [Feat] #61 - ListedProductModel 파일 추가

---
+++ SoBunSoBun/Views/SettleUp/SettleUp1stStep/SettleUp1stStepDataModel.swift
@@ -0,0 +1,15 @@
+//
+//  SettleUp1stStepDataModel.swift
+//  SoBunSoBun
+//
+//  Created by 허성필 on 12/27/25.
+//
+
+import Foundation
+
+struct ListedProductModel: Equatable {
+    let name: String
+    let count: Int
+    let price: Int
+    let unitIndex: Int
+}
