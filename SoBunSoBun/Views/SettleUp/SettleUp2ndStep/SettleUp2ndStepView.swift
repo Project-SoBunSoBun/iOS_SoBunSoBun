d42f7473f907c03cf8285cd9abcdcd2c9fe902ef
heopill <heopill@naver.com>
2026-01-17 02:02:50 +0900

    [Feat] #70 - SettleUp2ndStepView UI 구현
    서버 통신 전까지 UI 구현 완료

--- SoBunSoBun/Views/SettleUp/SettleUp2ndStep/SettleUp2ndStepView.swift
+++ SoBunSoBun/Views/SettleUp/SettleUp2ndStep/SettleUp2ndStepView.swift
@@ -54,6 +54,107 @@ class SettleUp2ndStepView: UIViewController {
         return button
     }()
     
+    // 전체 스크롤 뷰
+    private let scrollView: UIScrollView = {
+        let sv = UIScrollView()
+        sv.showsVerticalScrollIndicator = false
+        
+        return sv
+    }()
+    
+    // 스크롤 뷰가 들어갈 View
+    private let contentView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .backgroundWhite
+        
+        return view
+    }()
+    
+    // 2/3
+    private let stepLabel: UILabel = {
+        let lb = UILabel()
+        let attributedText = NSAttributedString(
+            string: "2/3",
+            attributes: title14.attributes(alignment: .left)
+        )
+        lb.attributedText = attributedText
+        lb.textColor = .primary400
+        
+        return lb
+    }()
+    
+    // 제목 라벨
+    private let titleLabel: UILabel = {
+        let lb = UILabel()
+        let attributedText = NSAttributedString(
+            string: String(localized: "SettleUpStart"),
+            attributes: title24.attributes(alignment: .left)
+        )
+        lb.attributedText = attributedText
+        lb.textColor = .neutral900
+        
+        return lb
+    }()
+    
+    // 등록된 상품 수량 라벨 배경 뷰
+    private let subtitleBackground: UIView = {
+        let view = UIView()
+        view.backgroundColor = .primary50
+        view.layer.cornerRadius = 14
+        view.clipsToBounds = true
+        
+        return view
+    }()
+    
+    // 등록된 상품 수량 라벨
+    private let subtitleLabel: UILabel = {
+        let lb = UILabel()
+        let format = String(localized: "SettleUpParticipant")
+        let attributedText = NSAttributedString(
+            string: String(format: format, 0),
+            attributes: body14.attributes(alignment: .center)
+        )
+        
+        lb.attributedText = attributedText
+        lb.textColor = .neutral800
+        lb.numberOfLines = 0
+        
+        return lb
+    }()
+    
+    // 참여자별 정산 컴포넌트가 들어갈 스택뷰
+    private let calculationStackView: UIStackView = {
+        let sv = UIStackView()
+        sv.axis = .vertical
+        sv.spacing = 8
+        
+        return sv
+    }()
+    
+    // 등록하기 버튼
+    private let registerButton = Button(title: String(localized: "Register"))
+    
+    private let test: CalculationGuest = {
+        let testProduct = ListedProductModel(name: "두쫀쿠", count: 1000, price: 5000000, unitIndex: 1)
+        let test = CalculationGuest(product: testProduct)
+        
+        return test
+    }()
+    
+    private let test2: CalculationGuest = {
+        let testProduct = ListedProductModel(name: "두쫀쿠", count: 500, price: 1500000, unitIndex: 2)
+        let test = CalculationGuest(product: testProduct)
+        
+        return test
+    }()
+    
+    private let test3: CalculationGuest = {
+        let testProduct = ListedProductModel(name: "배추", count: 5, price: 25000, unitIndex: 1)
+        let test = CalculationGuest(product: testProduct)
+        
+        return test
+    }()
+    
     // MARK: - 생명주기
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -74,15 +175,76 @@ class SettleUp2ndStepView: UIViewController {
     private func configureUI() {
         view.backgroundColor = .backgroundWhite
         
-        [backButton].forEach {
+        [backButton, scrollView].forEach {
             view.addSubview($0)
         }
+        
+        scrollView.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview()
+            make.top.equalTo(backButton.snp.bottom)
+            make.bottom.equalTo(view.keyboardLayoutGuide.snp.top)
+        }
+        
+        scrollView.addSubview(contentView)
+        
+        contentView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            make.width.equalToSuperview()
+        }
+        
+        [stepLabel, titleLabel, subtitleBackground, calculationStackView, registerButton].forEach {
+            contentView.addSubview($0)
+        }
+        
+        // CalculationGuest 컴포넌트가 들어갈 StackView
+        [test, test2, test3].forEach {
+            calculationStackView.addArrangedSubview($0)
+        }
+        
+        subtitleBackground.addSubview(subtitleLabel)
         
         backButton.snp.makeConstraints { make in
             make.leading.equalToSuperview().offset(4)
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
             make.size.equalTo(48)
         }
+        
+        stepLabel.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.top.equalToSuperview().offset(8)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.top.equalTo(stepLabel.snp.bottom).offset(8)
+        }
+        
+        subtitleBackground.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.top.equalTo(titleLabel.snp.bottom).offset(16)
+            make.height.equalTo(53)
+        }
+        
+        subtitleLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        
+        calculationStackView.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.top.equalTo(subtitleBackground.snp.bottom).offset(8)
+        }
+        
+        registerButton.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.top.equalTo(calculationStackView.snp.bottom).offset(16)
+            make.bottom.equalToSuperview()
+        }
+        
+        let mockParticipants = ["닉네임(나)", "닉네임 A", "닉네임 B", "닉네임 C", "닉네임 D"]
+        
+        test.setParticipants(mockParticipants)
+        test2.setParticipants(mockParticipants)
+        test3.setParticipants(mockParticipants)
     }
 }
 
