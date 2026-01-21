9da5a21ece750f1e61c4465ef5d0853367343513
heopill <heopill@naver.com>
2026-01-22 00:16:28 +0900

    [Feat] #75 - Review Component 구현

---
+++ SoBunSoBun/Views/Settings/SettingsComponents/Review.swift
@@ -0,0 +1,131 @@
+//
+//  Review.swift
+//  SoBunSoBun
+//
+//  Created by 허성필 on 1/21/26.
+//
+
+import UIKit
+import SnapKit
+
+class Review: UIView {
+    init(frame: CGRect = .zero, title: String) {
+        super.init(frame: frame)
+        
+        configure(title: title)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private let insets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    
+    // MARK: - 디자인 요소
+    // 이모지 뷰
+    private let emojiView: UIImageView = {
+        let v = UIImageView()
+        v.contentMode = .scaleAspectFit
+        
+        v.snp.makeConstraints { make in
+            make.size.equalTo(16)
+        }
+        
+        return v
+    }()
+    
+    // 텍스트 라벨
+    private let titleLabel: UILabel = {
+        let lb = UILabel()
+        
+        return lb
+    }()
+    
+    // 스택뷰
+    private let stackView: UIStackView = {
+        let sv = UIStackView()
+        sv.axis = .horizontal
+        sv.spacing = 8
+        sv.alignment = .center
+        
+        return sv
+    }()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layer.shadowPath = UIBezierPath(
+            roundedRect: bounds,
+            cornerRadius: 12
+        ).cgPath
+    }
+    
+    // MARK: - 레이아웃 설정
+    private func configure(title: String) {
+        self.backgroundColor = .backgroundWhite
+        
+        // 모서리
+        self.layer.cornerRadius = 12
+        
+        // 테두리
+        self.layer.borderWidth = 2
+        self.layer.borderColor = UIColor.primary50.cgColor
+        
+        // 그림자
+        self.layer.shadowOffset = .zero
+        self.layer.shadowColor = UIColor.primary300.withAlphaComponent(0.16).cgColor
+        self.layer.shadowOpacity = 1
+        self.layer.shadowRadius = 16
+        self.clipsToBounds = false
+        
+        [emojiView, titleLabel].forEach {
+            stackView.addArrangedSubview($0)
+        }
+        
+        self.addSubview(stackView)
+        
+        stackView.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(insets)
+        }
+        
+        setEmojiAndColor(reviewNumber: title)
+    }
+    
+    private func setEmojiAndColor(reviewNumber: String) {
+        let localizedString = NSLocalizedString(reviewNumber, comment: "")
+        let attributedText = NSAttributedString(
+            string: localizedString,
+            attributes: body16.attributes(alignment: .center)
+        )
+        titleLabel.attributedText = attributedText
+        
+        let suffix = String(reviewNumber.suffix(3))
+        guard let number = Int(suffix) else { return }
+        
+        switch number {
+        case 1:
+            emojiView.image = .emojiEightOclock
+            titleLabel.textColor = UIColor.review1
+            
+        case 2:
+            emojiView.image = .emojiGreenHeart
+            titleLabel.textColor = UIColor.review2
+            
+        case 3:
+            emojiView.image = .emojiThumbsUp
+            titleLabel.textColor = UIColor.review3
+            
+        case 4:
+            emojiView.image = .emojiGlowingStar
+            titleLabel.textColor = UIColor.review4
+            
+        case 5:
+            emojiView.image = .emojiGrinningFace
+            titleLabel.textColor = UIColor.review5
+            
+        default:
+            emojiView.image = .logo
+            titleLabel.textColor = UIColor.neutral900
+        }
+    }
+}
