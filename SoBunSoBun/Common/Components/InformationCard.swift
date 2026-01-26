4d5fd4ef9f6f8525a5d904a6ca63b4f2038ecd41
hugesilver <xor3160@naver.com>
2026-01-27 03:42:03 +0900

    [Refactor] #83 - InformationCard 컴포넌트 리팩토링

--- SoBunSoBun/Common/Components/InformationCard.swift
+++ SoBunSoBun/Common/Components/InformationCard.swift
@@ -9,18 +9,10 @@ import UIKit
 import SnapKit
 
 class InformationCard: UIView {
-    /// dateTimeString은 ISO 8601(DateTime) 형식의 문자열입니다.
-    init(frame: CGRect = .zero,
-         minCount: Int,
-         maxCount: Int,
-         location: String,
-         dateTimeString: String) {
+    override init(frame: CGRect) {
         super.init(frame: frame)
         
-        configure(minCount: minCount,
-                  maxCount: maxCount,
-                  location: location,
-                  dateTimeString: dateTimeString)
+        configure()
     }
     
     required init?(coder: NSCoder) {
@@ -38,123 +30,167 @@ class InformationCard: UIView {
         return sv
     }
     
-    // 제목 label 컴포넌트
-    private func titleLabel() -> UILabel {
-        let lb = UILabel()
-        lb.font = title16.font
-        lb.textColor = .neutral900
-        lb.numberOfLines = 1
-        lb.textAlignment = .left
+    private func titleAttributes() -> [NSAttributedString.Key: Any] {
+        var attributes: [NSAttributedString.Key: Any] = title16.attributes()
+        attributes[.foregroundColor] = UIColor.neutral900
         
-        return lb
+        return attributes
     }
     
-    // 설명 label 컴포넌트
-    private func descLabel() -> UILabel {
-        let lb = UILabel()
-        lb.font = body16.font
-        lb.textColor = .neutral700
-        lb.numberOfLines = 1
-        lb.textAlignment = .right
+    private func descAttributes() -> [NSAttributedString.Key: Any] {
+        var attributes: [NSAttributedString.Key: Any] = body16.attributes(alignment: .right)
+        attributes[.foregroundColor] = UIColor.neutral700
         
-        return lb
+        return attributes
     }
     
     // 모집 인원
+    var minMembers: Int = 0 {
+        didSet {
+            setParticipants()
+        }
+    }
+    
+    var maxMembers: Int = 0 {
+        didSet {
+            setParticipants()
+        }
+    }
+    
     private lazy var participantsStackView: UIStackView = horizontalStackView()
     private lazy var participantsTitleLabel: UILabel = {
-        let lb = titleLabel()
-        lb.text = String(localized: "NumberOfParticipants")
+        let lb = UILabel()
+        lb.attributedText = NSAttributedString(string: String(localized: "NumberOfParticipants", table: "Home"), attributes: titleAttributes())
         
         return lb
     }()
-    private lazy var participantsDescLabel: UILabel = descLabel()
+    private lazy var participantsDescLabel: UILabel = UILabel()
+    
+    private func setParticipants() {
+        guard minMembers > 0, maxMembers > 0 else { return }
+        
+        let string: String = String(format: NSLocalizedString("ParticipantCount", tableName: "Home", comment: "participants minimum and maximum counts"), minMembers, maxMembers)
+        
+        participantsDescLabel.attributedText = NSAttributedString(string: string, attributes: descAttributes())
+    }
     
     // 지점 위치
-    private lazy var meetingLocationStackView: UIStackView = horizontalStackView()
-    private lazy var meetingLocationTitleLabel: UILabel = {
-        let lb = titleLabel()
-        lb.text = String(localized: "MeetingLocation")
+    var locationName: String? {
+        didSet {
+            setLocationName()
+        }
+    }
+    
+    private lazy var locationStackView: UIStackView = horizontalStackView()
+    private lazy var locationTitleLabel: UILabel = {
+        let lb = UILabel()
+        lb.attributedText = NSAttributedString(string: String(localized: "MeetingLocation", table: "Home"), attributes: titleAttributes())
         
         return lb
     }()
-    private lazy var meetingLocationDescLabel: UILabel = descLabel()
+    private lazy var locationDescLabel: UILabel = UILabel()
+    
+    private func setLocationName() {
+        let string: String = locationName ?? String(localized: "Unknown", table: "Common")
+        
+        locationDescLabel.attributedText = NSAttributedString(string: string, attributes: descAttributes())
+    }
     
     // 날짜 및 시간
+    var meetAt: String? {
+        didSet {
+            setMeetAt()
+        }
+    }
+    
     private lazy var dateTimeStackView: UIStackView = horizontalStackView()
     private lazy var dateTimeTitleLabel: UILabel = {
-        let lb = titleLabel()
-        lb.text = String(localized: "DateTime")
+        let lb = UILabel()
+        lb.attributedText = NSAttributedString(string: String(localized: "DateTime", table: "Home"), attributes: titleAttributes())
         
         return lb
     }()
-    private lazy var dateTimeDescLabel: UILabel = descLabel()
+    private lazy var dateTimeDescLabel: UILabel = UILabel()
+    
+    private func setMeetAt() {
+        guard let meetAt else { return }
+        
+        let string: String = ISO8601ToLocalizedDateTimeString(meetAt)
+        
+        dateTimeDescLabel.attributedText = NSAttributedString(string: string, attributes: descAttributes())
+    }
     
     // 마감일
+    var deadlineAt: String? {
+        didSet {
+            setDeadLine()
+        }
+    }
+    
     private lazy var deadlineStackView: UIStackView = horizontalStackView()
     private lazy var deadlineTitleLabel: UILabel = {
-        let lb = titleLabel()
-        lb.text = String(localized: "Deadline")
+        let lb = UILabel()
+        lb.attributedText = NSAttributedString(string: String(localized: "Deadline", table: "Home"), attributes: titleAttributes())
         
         return lb
     }()
-    private lazy var deadlineDescLabel: UILabel = descLabel()
+    private lazy var deadlineDescLabel: UILabel = UILabel()
     
-    // MARK: - 레이아웃 설정
-    private func configure(
-        minCount: Int,
-        maxCount: Int,
-        location: String,
-        dateTimeString: String) {
-            self.backgroundColor = .primary50
-            self.layer.cornerRadius = 16
-            self.clipsToBounds = true
-            
-            // 모집 인원
-            participantsStackView.addArrangedSubview(participantsTitleLabel)
-            participantsStackView.addArrangedSubview(participantsDescLabel)
-            participantsDescLabel.text = String(format: NSLocalizedString("ParticipantCount", tableName: "Home", comment: "participants minimum and maximum counts"), minCount, maxCount)
-            
-            addSubview(participantsStackView)
-            
-            participantsStackView.snp.makeConstraints { make in
-                make.horizontalEdges.top.equalToSuperview().inset(16)
-            }
-            
-            // 지점 위치
-            meetingLocationStackView.addArrangedSubview(meetingLocationTitleLabel)
-            meetingLocationStackView.addArrangedSubview(meetingLocationDescLabel)
-            meetingLocationDescLabel.text = location
-            
-            addSubview(meetingLocationStackView)
-            
-            meetingLocationStackView.snp.makeConstraints { make in
-                make.horizontalEdges.equalToSuperview().inset(16)
-                make.top.equalTo(participantsStackView.snp.bottom).offset(8)
-            }
-            
-            // 날짜 및 시간
-            dateTimeStackView.addArrangedSubview(dateTimeTitleLabel)
-            dateTimeStackView.addArrangedSubview(dateTimeDescLabel)
-            dateTimeDescLabel.text = ISO8601ToLocalizedDateTimeString(dateTimeString)
-            
-            addSubview(dateTimeStackView)
-            
-            dateTimeStackView.snp.makeConstraints { make in
-                make.horizontalEdges.equalToSuperview().inset(16)
-                make.top.equalTo(meetingLocationStackView.snp.bottom).offset(8)
-            }
-            
-            // 마감일
-            deadlineStackView.addArrangedSubview(deadlineTitleLabel)
-            deadlineStackView.addArrangedSubview(deadlineDescLabel)
-            deadlineDescLabel.text = ISO8601ToDDay(dateTimeString)
-            
-            addSubview(deadlineStackView)
-            
-            deadlineStackView.snp.makeConstraints { make in
-                make.horizontalEdges.bottom.equalToSuperview().inset(16)
-                make.top.equalTo(dateTimeStackView.snp.bottom).offset(8)
-            }
+    private func setDeadLine() {
+        guard let deadlineAt else { return }
+        
+        let string: String = ISO8601ToDDay(deadlineAt)
+        
+        deadlineDescLabel.attributedText = NSAttributedString(string: string, attributes: descAttributes())
+    }
+    
+    // MARK: - UI 설정
+    private func configure() {
+        self.backgroundColor = .primary50
+        self.layer.cornerRadius = 16
+        self.clipsToBounds = true
+        
+        // 모집 인원
+        participantsStackView.addArrangedSubview(participantsTitleLabel)
+        participantsStackView.addArrangedSubview(participantsDescLabel)
+        
+        addSubview(participantsStackView)
+        
+        participantsStackView.snp.makeConstraints { make in
+            make.horizontalEdges.top.equalToSuperview().inset(16)
         }
+        
+        // 지점 위치
+        locationStackView.addArrangedSubview(locationTitleLabel)
+        locationStackView.addArrangedSubview(locationDescLabel)
+        
+        addSubview(locationStackView)
+        
+        locationStackView.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.top.equalTo(participantsStackView.snp.bottom).offset(8)
+        }
+        
+        // 날짜 및 시간
+        dateTimeStackView.addArrangedSubview(dateTimeTitleLabel)
+        dateTimeStackView.addArrangedSubview(dateTimeDescLabel)
+        
+        addSubview(dateTimeStackView)
+        
+        dateTimeStackView.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.top.equalTo(locationStackView.snp.bottom).offset(8)
+        }
+        
+        // 마감일
+        deadlineStackView.addArrangedSubview(deadlineTitleLabel)
+        deadlineStackView.addArrangedSubview(deadlineDescLabel)
+        
+        addSubview(deadlineStackView)
+        
+        deadlineStackView.snp.makeConstraints { make in
+            make.horizontalEdges.bottom.equalToSuperview().inset(16)
+            make.top.equalTo(dateTimeStackView.snp.bottom).offset(8)
+        }
+    }
 }
