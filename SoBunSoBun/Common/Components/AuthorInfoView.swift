9162aa2135bb5ea8bba51e43dd3e1e4ed5c685f9
hugesilver <xor3160@naver.com>
2026-01-27 19:18:31 +0900

    [Refactor] #83 - AuthorInfoView 컴포넌트 리팩토링

--- SoBunSoBun/Common/Components/AuthorInfoView.swift
+++ SoBunSoBun/Common/Components/AuthorInfoView.swift
@@ -13,7 +13,11 @@ import RxGesture
 import OSLog
 
 class AuthorInfoView: UIStackView {
-    var id: Int?
+    var id: Int? {
+        didSet {
+            bind()
+        }
+    }
     
     private let logger = Logger(
         subsystem: "SoBunSoBun",
@@ -23,26 +27,7 @@ class AuthorInfoView: UIStackView {
     private let disposeBag = DisposeBag()
     
     private let UNKNOWN_STRING = String(localized: "Unknown", table: "Common")
-    
-    /// createdAt은 필수 요소입니다.
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-        configureUI()
-        bind()
-    }
-    
-    required init(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     // MARK: - 디자인 요소
-    var profileImageUrl: String? {
-        didSet {
-            setProfileImage()
-        }
-    }
-    
     private let profileImageView: UIImageView = {
         let iv = UIImageView()
         iv.contentMode = .scaleAspectFill
@@ -58,8 +43,60 @@ class AuthorInfoView: UIStackView {
         return iv
     }()
     
-    private func setProfileImage() {
-        if let profileImageUrl = profileImageUrl {
+    private let verticalStackView: UIStackView = {
+        let sv = UIStackView()
+        sv.axis = .vertical
+        sv.spacing = 4
+        sv.alignment = .leading
+        
+        return sv
+    }()
+    
+    private let nicknameLabel: UILabel = {
+        let lb = UILabel()
+        lb.numberOfLines = 1
+        
+        return lb
+    }()
+    
+    private let bottomInfoLabel: UILabel = {
+        let lb = UILabel()
+        lb.numberOfLines = 1
+        
+        return lb
+    }()
+    
+    // MARK: - UI 설정
+    private func configureUI(
+        profileImageUrl: String?,
+        nickname: String?,
+        createdAt: String?,
+        verifyLocation: String?
+    ) {
+        self.axis = .horizontal
+        self.spacing = 8
+        self.alignment = .center
+        
+        addArrangedSubview(profileImageView)
+        
+        setProfileImage(profileImageUrl)
+        
+        addArrangedSubview(verticalStackView)
+        
+        [nicknameLabel, bottomInfoLabel].forEach {
+            verticalStackView.addArrangedSubview($0)
+        }
+        
+        setNickname(nickname)
+        setBottomInfo(createdAt, verifyLocation: verifyLocation)
+        
+        profileImageView.setContentHuggingPriority(.required, for: .horizontal)
+        nicknameLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        bottomInfoLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
+    }
+    
+    private func setProfileImage(_ profileImageUrl: String?) {
+        if let profileImageUrl {
             let imageUrl = URL(string: API_URL + profileImageUrl)
             
             profileImageView.kf.setImage(
@@ -83,82 +120,28 @@ class AuthorInfoView: UIStackView {
         }
     }
     
-    private let verticalStackView: UIStackView = {
-        let sv = UIStackView()
-        sv.axis = .vertical
-        sv.spacing = 4
-        sv.alignment = .leading
-        
-        return sv
-    }()
-    
-    var nickname: String? {
-        didSet {
-            setNickname()
-        }
-    }
-    
-    private let nicknameLabel: UILabel = {
-        let lb = UILabel()
-        lb.numberOfLines = 1
-        
-        return lb
-    }()
-    
-    private func setNickname() {
+    private func setNickname(_ nickname: String?) {
         var nicknameAttributes: [NSAttributedString.Key: Any] = title16.attributes()
         nicknameAttributes[.foregroundColor] = UIColor.neutral900
         
         nicknameLabel.attributedText = NSAttributedString(string: nickname ?? UNKNOWN_STRING, attributes: nicknameAttributes)
     }
     
-    var createdAt: String? {
-        didSet {
-            setBottomInfo()
-        }
-    }
-    
-    var verifyLocation: String? {
-        didSet {
-            setBottomInfo()
-        }
-    }
-    
-    private let bottomInfoLabel: UILabel = {
-        let lb = UILabel()
-        lb.numberOfLines = 1
-        
-        return lb
-    }()
-    
-    private func setBottomInfo() {
-        guard let createdAt = createdAt else { return }
-        
+    private func setBottomInfo(_ createdAt: String?, verifyLocation: String?) {
         var bottomInfoAttributes: [NSAttributedString.Key: Any] = body14.attributes()
         bottomInfoAttributes[.foregroundColor] = UIColor.neutral500
         
-        let createdAtString: String = ISO8601ToRelativeString(createdAt)
+        let createdAtString: String
+        
+        if let createdAt {
+            createdAtString = ISO8601ToRelativeString(createdAt)
+        } else {
+            createdAtString = UNKNOWN_STRING
+        }
+        
         let verifyLocationString: String = "\(verifyLocation ?? UNKNOWN_STRING) \(String(localized: "Verified", table: "Home"))"
         
         bottomInfoLabel.attributedText = NSAttributedString(string: "\(createdAtString) | \(verifyLocationString) ", attributes: bottomInfoAttributes)
-    }
-    
-    // MARK: - UI 설정
-    private func configureUI() {
-        self.axis = .horizontal
-        self.spacing = 8
-        self.alignment = .center
-        
-        addArrangedSubview(profileImageView)
-        addArrangedSubview(verticalStackView)
-        
-        [nicknameLabel, bottomInfoLabel].forEach {
-            verticalStackView.addArrangedSubview($0)
-        }
-        
-        profileImageView.setContentHuggingPriority(.required, for: .horizontal)
-        nicknameLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
-        bottomInfoLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
     }
 }
 
@@ -177,7 +160,7 @@ extension AuthorInfoView {
         .subscribe(onNext: { [weak self] _ in
             guard let self = self else { return }
             
-            // TODO: 프로필 이동
+            // TODO: 프로필 이동 구현
         })
         .disposed(by: disposeBag)
     }
