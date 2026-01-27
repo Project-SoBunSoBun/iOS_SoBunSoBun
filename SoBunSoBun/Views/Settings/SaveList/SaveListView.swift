fab74b9a177ebb2d465480cf9e2a04dba18a8282
heopill <heopill@naver.com>
2026-01-28 02:10:47 +0900

    [Feat] #86 - 프로필 설정, 카드 세팅 버튼 클릭 시 이동 할 View 추가

---
+++ SoBunSoBun/Views/Settings/SaveList/SaveListView.swift
@@ -0,0 +1,29 @@
+//
+//  SaveListView.swift
+//  SoBunSoBun
+//
+//  Created by 허성필 on 1/28/26.
+//
+
+import UIKit
+
+class SaveListView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
