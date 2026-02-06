c4f018732bd507087a2fffb2e9be51b61fe6dd73
heopill <heopill@naver.com>
2026-02-07 02:47:27 +0900

    [Feat] #106 - 고객 지원 화면 구현하기 및 다음 뷰 연결

---
+++ SoBunSoBun/Views/Settings/BugReport/BugReportView.swift
@@ -0,0 +1,57 @@
+//
+//  BugReportView.swift
+//  SoBunSoBun
+//
+//  Created by 허성필 on 2/7/26.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+import ReactorKit
+import OSLog
+
+class BugReportView: UIViewController {
+    private let logger = Logger(
+        subsystem: "SoBunSoBun",
+        category: "BugReport.View"
+    )
+    
+    //    typealias Reactor = BugReportReactor
+    //    private let reactor = BugReportReactor()
+    
+    private let disposeBag = DisposeBag()
+    
+    // MARK: - 디자인 요소
+    // 상단 네비게이션 바
+    private lazy var topNavigationBar: TopNavigationBar = {
+        let tnb = TopNavigationBar()
+        tnb.title = String(localized: "BugReport", table: "Settings")
+        tnb.parentViewController = self
+        
+        return tnb
+    }()
+    
+    // MARK: - 생명주기
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+    }
+    
+    // MARK: - 레이아웃 설정
+    private func configureUI() {
+        view.backgroundColor = .backgroundWhite
+        
+        [topNavigationBar].forEach {
+            view.addSubview($0)
+        }
+        
+        // 탑 네비게이션 바
+        topNavigationBar.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+        }
+    }
+}
