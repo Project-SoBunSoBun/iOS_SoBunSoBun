e895e598bacf00cc08fdf601f4b3264adaed7762
heopill <heopill@naver.com>
2026-02-13 01:34:41 +0900

    [Refactor] #124 - SettingAPIs로 분리

--- SoBunSoBun/Views/Settings/_NetworkManager/SettingAPIs.swift
+++ SoBunSoBun/Views/Settings/_NetworkManager/SettingAPIs.swift
@@ -11,6 +11,10 @@ import Moya
 enum SettingAPIs {
     // 탈퇴
     case postWithdraw(reasonCode: String, reasonDetail: String, agreedToTerms: Bool)
+    // 마이페이지
+    case getMeProfile
+    case patchProfileImage(profileImage: Data)
+    case patchNickname(nickname: String)
 }
 
 extension SettingAPIs: TargetType {
@@ -27,6 +31,12 @@ extension SettingAPIs: TargetType {
         switch self {
         case .postWithdraw:
             return "users/me/withdraw"
+        case .getMeProfile:
+            return "api/me/profile"
+        case .patchProfileImage:
+            return "users/me/profile-image"
+        case .patchNickname:
+            return "users/me/nickname"
         }
     }
     
@@ -35,6 +45,13 @@ extension SettingAPIs: TargetType {
         case // POST
                 .postWithdraw:
             return .post
+        case // GET
+                .getMeProfile:
+            return .get
+        case // PATCH
+                .patchProfileImage,
+                .patchNickname:
+            return .patch
         }
     }
     
@@ -44,10 +61,35 @@ extension SettingAPIs: TargetType {
             let model = WithdrawRequestBodyModel(reasonCode: reasonCode, reasonDetail: reasonDetail, agreedToTerms: agreedToTerms)
             
             return .requestJSONEncodable(model)
+        case .getMeProfile:
+            return .requestPlain
+            
+        case .patchProfileImage(let profileImage):
+            let imageData = profileImage
+            var formData: [MultipartFormData] = []
+            
+            formData.append(MultipartFormData(
+                provider: .data(imageData),
+                name: "profileImage",
+                fileName: "profile.jpg",
+                mimeType: "image/jpeg"
+            ))
+            
+            return .uploadMultipart(formData)
+            
+        case .patchNickname(nickname: let nickname):
+            let body: [String: String] = ["nickname": nickname]
+            
+            return .requestJSONEncodable(body)
         }
     }
     
     var headers: [String : String]? {
-        return [:]
+        switch self {
+        case .patchProfileImage:
+            return ["Content-Type": "multipart/form-data"]
+        default :
+            return [:]
+        }
     }
 }
