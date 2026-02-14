1971bd8aacb2ccdba420d5aa1cb3d4c184ba0c2a
heopill <heopill@naver.com>
2026-02-15 02:28:48 +0900

    [Feat] #133 - 공지사항 디테일 화면 구현

---
+++ SoBunSoBun/Views/Settings/Announcement/AnnouncementDetail/AnnouncementDetailReactor.swift
@@ -0,0 +1,77 @@
+//
+//  AnnouncementDetailReactor.swift
+//  SoBunSoBun
+//
+//  Created by 허성필 on 2/14/26.
+//
+
+import ReactorKit
+import RxSwift
+import OSLog
+
+class AnnouncementDetailReactor: Reactor {
+    private let logger = Logger(
+        subsystem: "SoBunSoBun",
+        category: "Settings.AnnouncementDetail.Reactor"
+    )
+    
+    init(id: Int) {
+        initialState = State(id: id)
+    }
+    
+    let initialState: State
+    
+    private let networkManager = SettingNetworkManager()
+    
+    enum Action {
+        case viewDidLoad
+    }
+    
+    enum Mutation {
+        case setError(String)
+        case setNoticeDetail(AnnouncementDetailDataModel)
+    }
+    
+    struct State {
+        var noticeDetail: AnnouncementDetailDataModel?
+        let id: Int
+        @Pulse var errorMessage: String?
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+            
+        case .viewDidLoad:
+            loadNoticeDetail(id: currentState.id)
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        switch mutation {
+            
+        case .setError(let message):
+            newState.errorMessage = message
+            
+        case .setNoticeDetail(let noticeDetail):
+            newState.noticeDetail = noticeDetail
+        }
+        
+        return newState
+    }
+    
+    private func loadNoticeDetail(id: Int) -> Observable<Mutation> {
+        return networkManager.getAnnouncementsDetail(id: id)
+            .asObservable()
+            .flatMap { response -> Observable<Mutation> in
+                self.logger.debug("공지사항 상세 조회 성공")
+                
+                return Observable.just(.setNoticeDetail(response.data))
+            }
+            .catch { error in
+                self.logger.fault("공지사항 상세 조회 실패: \(error.localizedDescription)")
+                
+                return Observable.just(.setError(error.localizedDescription))
+            }
+    }
+}
---
+++ SoBunSoBun/Views/Settings/Announcement/AnnouncementDetail/AnnouncementDetailView.swift
@@ -0,0 +1,138 @@
+//
+//  AnnouncementDetailView.swift
+//  SoBunSoBun
+//
+//  Created by 허성필 on 2/14/26.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+import ReactorKit
+import OSLog
+
+class AnnouncementDetailView: UIViewController {
+    init(model: AnnouncementContentModel, nibName nibNameOrNil: String? = nil
+         , bundle nibBundleOrNil: Bundle? = nil) {
+        reactor = AnnouncementDetailReactor(id: model.id)
+        
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        titleView.configure(item: model)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    typealias Reactor = AnnouncementDetailReactor
+    private let reactor: AnnouncementDetailReactor
+    
+    private let disposeBag = DisposeBag()
+    
+    // MARK: - 디자인 요소
+    private lazy var topNavigationBar: TopNavigationBar = {
+        let tnb = TopNavigationBar()
+        tnb.parentViewController = self
+        
+        return tnb
+    }()
+    
+    private let scrollView = UIScrollView()
+    
+    private let contentView = UIView()
+    
+    private let titleView = AnnouncementCellView()
+    
+    private let detailLabel: UILabel = {
+        let lb = UILabel()
+        lb.numberOfLines = 0
+        
+        return lb
+    }()
+
+    // MARK: - 생명주기
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+        bind(reactor: reactor)
+    }
+    
+    // MARK: - 레이아웃 설정
+    private func configureUI() {
+        view.backgroundColor = .backgroundWhite
+        
+        [topNavigationBar, scrollView].forEach {
+            view.addSubview($0)
+        }
+        
+        topNavigationBar.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+        }
+        
+        scrollView.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview()
+            make.top.equalTo(topNavigationBar.snp.bottom)
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+        }
+        
+        scrollView.addSubview(contentView)
+        
+        contentView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            make.width.equalToSuperview()
+        }
+        
+        [titleView, detailLabel].forEach {
+            contentView.addSubview($0)
+        }
+        
+        titleView.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview()
+            make.top.equalToSuperview()
+        }
+        
+        detailLabel.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.top.equalTo(titleView.snp.bottom).offset(16)
+            make.bottom.equalToSuperview().inset(48)
+        }
+    }
+    
+    private func setDetailLabel(content: String) {
+        var attributes: [NSAttributedString.Key: Any] = body16.attributes(alignment: .left)
+        attributes[.foregroundColor] = UIColor.neutral900
+        
+        detailLabel.attributedText = NSAttributedString(
+            string: content,
+            attributes: attributes
+        )
+    }
+}
+
+extension AnnouncementDetailView {
+    private func bind(reactor: AnnouncementDetailReactor) {
+        bindAction(reactor: reactor)
+        bindState(reactor: reactor)
+    }
+    
+    private func bindAction(reactor: AnnouncementDetailReactor) {
+        reactor.action.onNext(.viewDidLoad)
+    }
+    
+    private func bindState(reactor: AnnouncementDetailReactor) {
+        reactor.state.map { $0.noticeDetail }
+            .compactMap { $0 }
+            .distinctUntilChanged()
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] response in
+                guard let self = self else { return }
+                
+                self.setDetailLabel(content: response.content)
+            })
+            .disposed(by: disposeBag)
+    }
+}
