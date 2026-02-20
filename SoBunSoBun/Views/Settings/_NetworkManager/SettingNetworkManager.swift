425b9fa566f8ee0748e3e9126b0c671e585f9858
heopill <heopill@naver.com>
2026-02-21 03:48:35 +0900

    [Feat] #143 - SettingNetworkManager에 1:1 문의 전송 함수 추가

--- SoBunSoBun/Views/Settings/_NetworkManager/SettingNetworkManager.swift
+++ SoBunSoBun/Views/Settings/_NetworkManager/SettingNetworkManager.swift
@@ -72,4 +72,18 @@ class SettingNetworkManager {
         .filterSuccessfulStatusCodes()
         .map { _ in () }
     }
+    
+    // MARK: - 1:1 문의
+    // 1:1 문의 전송
+    func postInquiries(typeCode: String, content:String, replyEmail: String, selectedImages: [UIImage]?) -> Single<Void> {
+        let imageDatas: [Data]? = selectedImages?.compactMap {
+            $0.jpegData(compressionQuality: 0.7)
+        }
+
+        return authProvider.rx.request(
+            MultiTarget(SettingAPIs.postInquiries(typeCode: typeCode, content: content, replyEmail: replyEmail, selectedImages: imageDatas))
+        )
+        .filterSuccessfulStatusCodes()
+        .map { _ in () }
+    }
 }
