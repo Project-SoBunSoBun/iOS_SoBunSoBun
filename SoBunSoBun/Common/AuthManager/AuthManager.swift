44d512c059988a38e892b4537946e30dc1a5596c
heopill <heopill@naver.com>
2026-02-27 02:10:50 +0900

    [Feat] #148 - AuthManager에 AppleLogin 관련 함수 추가

--- SoBunSoBun/Common/AuthManager/AuthManager.swift
+++ SoBunSoBun/Common/AuthManager/AuthManager.swift
@@ -9,6 +9,7 @@ import Foundation
 import UIKit
 import OSLog
 import KakaoSDKUser
+import RxSwift
 
 class AuthManager {
     private let logger = Logger(
@@ -17,11 +18,45 @@ class AuthManager {
     )
     
     static let shared = AuthManager()
+    private let appleLoginManager = AppleLoginManager()
+    private let networkManager = SignInNetworkManager()
+    
+    private let disposeBag = DisposeBag()
+    
+    private let loginType = KeyChain.shared.get(key: "LOGIN_TYPE")
     
     private init() {}
     
+    func checkAppleAuthentication() {
+        guard let userID = KeyChain.shared.get(key: "APPLE_USER_ID"),
+              let loginType = KeyChain.shared.get(key: "LOGIN_TYPE"), loginType == "APPLE" else { return }
+        
+        appleLoginManager.checkAppleIDCredentialState(userID: userID)
+            .subscribe(onNext: { [weak self] state in
+                guard let self = self else { return }
+                
+                switch state {
+                case .authorized:
+                    self.logger.debug("애플 로그인 인증 상태: 연결됨")
+                    
+                case .revoked, .notFound:
+                    self.logger.error("애플 로그인 인증 상태: 연결 끊김 또는 찾을 수 없음. 로그아웃 진행")
+                    self.withdraw()
+                    
+                default:
+                    break
+                }
+            }, onError: { error in
+                self.logger.error("애플 인증 상태 확인 실패: \(error.localizedDescription)")
+            })
+            .disposed(by: disposeBag)
+    }
+    
     func logout() {
-        kakaoLogout()
+        if loginType == "KAKAO" {
+            kakaoLogout()
+        }
+        
         removeTokens()
         showLogOutAlert()
         
@@ -29,7 +64,12 @@ class AuthManager {
     }
     
     func withdraw() {
-        kakaoUnlink()
+        if loginType == "KAKAO" {
+            kakaoUnlink()
+        } else if loginType == "APPLE" {
+            appleRevoke()
+        }
+        
         removeTokens()
         switchToLoginView()
     }
@@ -43,6 +83,8 @@ class AuthManager {
         KeyChain.shared.remove(key: "USER_ID")
         KeyChain.shared.remove(key: "FCM_TOKEN")
         KeyChain.shared.remove(key: "EMAIL")
+        KeyChain.shared.remove(key: "LOGIN_TYPE")
+        KeyChain.shared.remove(key: "APPLE_USER_ID")
         
         logger.debug("모든 keychain 내 토큰 제거")
     }
@@ -96,5 +138,20 @@ class AuthManager {
                 self.logger.error("카카오 연결 끊기 실패: \(error)")
             }
         }
+    }
+    
+    private func appleRevoke() {
+        // 서버에 authorizationCode를 보내 애플 Revoke API 진행
+        networkManager.fetchAuthRevokeApple()
+            .subscribe(onSuccess: { [weak self] in
+                guard let self = self else { return }
+                
+                self.logger.debug("애플 Revoke 성공")
+            }, onFailure: { [weak self] error in
+                guard let self = self else { return }
+                
+                self.logger.error("애플 Revoke 실패: \(error.localizedDescription)")
+            })
+            .disposed(by: disposeBag)
     }
 }
