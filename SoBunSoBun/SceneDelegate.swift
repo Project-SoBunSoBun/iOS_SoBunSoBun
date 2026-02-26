1bae5ac48b470115929a5b5d83304e58dc771e00
heopill <heopill@naver.com>
2026-02-27 02:14:21 +0900

    [Feat] #148 - SceneDelegate에 애플 계정 연결 상태 체크 추가

--- SoBunSoBun/SceneDelegate.swift
+++ SoBunSoBun/SceneDelegate.swift
@@ -81,8 +81,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
     
     func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        // 애플 계정 연결 상태 체크
+        AuthManager.shared.checkAppleAuthentication()
     }
     
     func sceneWillResignActive(_ scene: UIScene) {
