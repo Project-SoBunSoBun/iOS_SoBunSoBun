ccc60a933f51544cdc7ff89e0c5e30d68386a5d9
heopill <heopill@naver.com>
2026-03-14 23:15:58 +0900

    [Refactor] #161 - SettleUpDataModel 수정

--- SoBunSoBun/Views/SettleUp/SettleUp/SettleUpDataModel.swift
+++ SoBunSoBun/Views/SettleUp/SettleUp/SettleUpDataModel.swift
@@ -9,6 +9,7 @@ import Foundation
 
 struct SettleUpItemModel {
     let settlementId, authorId: Int
+    let isAuthor: Bool
     let settlementStatus: Bool
     let title: String
     let location: String
