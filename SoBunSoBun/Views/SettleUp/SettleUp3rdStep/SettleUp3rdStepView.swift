c0a5f3a02e20dd7d9f26b7db30c2ce5cdf5e04d4
heopill <heopill@naver.com>
2026-03-17 19:30:04 +0900

    [Refacor] #168 - SettleUp3rdStepView 코드 수정

--- SoBunSoBun/Views/SettleUp/SettleUp3rdStep/SettleUp3rdStepView.swift
+++ SoBunSoBun/Views/SettleUp/SettleUp3rdStep/SettleUp3rdStepView.swift
@@ -112,6 +112,12 @@ class SettleUp3rdStepView: UIViewController {
         return view
     }()
     
+    private let numberFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        return formatter
+    }()
+    
     // MARK: - 생명주기
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -172,10 +178,7 @@ class SettleUp3rdStepView: UIViewController {
     
     // model에서 받은 총 금액을 subtitleLabel에 표시
     private func setSubtitleLabel(totalAmount: Int) {
-        let formatter = NumberFormatter()
-        formatter.numberStyle = .decimal
-        
-        let formattedAmount = formatter.string(from: NSNumber(value: totalAmount)) ?? "0"
+        let formattedAmount = numberFormatter.string(from: NSNumber(value: totalAmount)) ?? "0"
         
         let subtitleText = String.localizedStringWithFormat(
             String(localized: "TotalSettlementAmountFormat", table: "SettleUp"),
