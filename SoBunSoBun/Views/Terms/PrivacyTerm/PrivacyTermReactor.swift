66388425cc5f34ebe5aaa48e9a992e2cedec1eb9
heopill <heopill@naver.com>
2026-03-20 01:39:32 +0900

    [Feat] #180 - 개인정보처리 방침 화면 구현

--- SoBunSoBun/Views/Terms/PrivacyTerm/PrivacyTermReactor.swift
+++ SoBunSoBun/Views/Terms/PrivacyTerm/PrivacyTermReactor.swift
@@ -17,29 +17,54 @@ class PrivacyTermReactor: Reactor {
     
     let initialState = State()
     
+    private let networkManager = SettingNetworkManager()
+    
     enum Action {
-        
+        case viewDidLoad
     }
     
     enum Mutation {
-        
+        case setContent(String)
+        case setError(String)
     }
     
     struct State {
-        
+        var content: String = ""
+        @Pulse var errorMessage: String?
     }
     
     func mutate(action: Action) -> Observable<Mutation> {
         switch action {
-            
+        case .viewDidLoad:
+            loadTerms()
         }
     }
     
     func reduce(state: State, mutation: Mutation) -> State {
         var newState = state
-        switch mutation {    
+        
+        switch mutation {
+        case .setContent(let content):
+            newState.content = content
             
+        case .setError(let message):
+            newState.errorMessage = message
         }
         return newState
+    }
+    
+    private func loadTerms() -> Observable<Mutation> {
+        return networkManager.getTermsPrivacy()
+            .asObservable()
+            .flatMap { model -> Observable<Mutation> in
+                return Observable.just(.setContent(model.data.content))
+            }
+            .catch { [weak self] error in
+                guard let self = self else { return Observable.empty() }
+                
+                self.logger.critical("개인정보처리 방침 조회 실패: \(error.localizedDescription)")
+                
+                return Observable.just(.setError(String(localized: "FailToLoadPrivacyTerm", table: "Settings")))
+            }
     }
 }
