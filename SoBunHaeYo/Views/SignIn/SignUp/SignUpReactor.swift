ad62e3512873598f41b479f3292d71503cb49376
heopill <heopill@naver.com>
2026-04-02 03:11:07 +0900

    [Refactor] #222 - 3차 PR 피드백 반영 코드 수정

--- SoBunHaeYo/Views/SignIn/SignUp/SignUpReactor.swift
+++ SoBunHaeYo/Views/SignIn/SignUp/SignUpReactor.swift
@@ -46,7 +46,7 @@ class SignUpReactor: Reactor {
         @Pulse var shouldPopViewController: Void?
         @Pulse var shouldTermsDetail: String?
         @Pulse var signUpCompleted: Bool = false
-        @Pulse var signUpErrorMessage: String?
+        @Pulse var ErrorMessage: String?
         @Pulse var shouldRequestLocationPermission: Bool = false
         @Pulse var shouldShowLocationSettingAlert: Bool = false
         
@@ -139,7 +139,7 @@ class SignUpReactor: Reactor {
             newState.signUpCompleted = true
             
         case .setErrorMessage(let message):
-            newState.signUpErrorMessage = message
+            newState.ErrorMessage = message
             
         case .setRequestLocationPermission:
             newState.shouldRequestLocationPermission = true
